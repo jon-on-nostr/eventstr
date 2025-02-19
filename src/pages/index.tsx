d20cb8f0864c1ef3c3985d38dbcd805e52dc6550
Jon <j.g.mcalpine@gmail.com>
2025-02-19 11:20:42 -0800

    Init NextJS project with tailwind and typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-3xl">Eventstr</h1>
+      </main>
+    </div>
+  );
+}
