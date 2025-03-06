322c8aab736c6c5271d430678f52f86a494e7477
Jon <j.g.mcalpine@gmail.com>
2025-03-06 10:34:24 -0800

    Update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -87,7 +87,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>Nostr Learn - Educational Platform</title>
+        <title>eVENTSTR</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
